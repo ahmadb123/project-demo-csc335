e45559bbbc8d7f406b1efa031d67adb3577b859e
Ahmad Bishara <ahmadbishara@Ahmads-iMac.local>
2025-02-17 16:37:07 -0500

    get username from Async storage

--- HomePage.js
+++ HomePage.js
@@ -1,15 +1,37 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, View, Text } from 'react-native';
 import styles from './assets/HomePageStyles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // import { useNavigation } from '@react-navigation/native';
 
+const getUsername = async (value) => {
+  try {
+    const data = await AsyncStorage.getItem(value);
+    return data;
+  } catch (e) {
+    console.error("Error", e);
+  }
+};
+
 function HomePage() {
+
+  const [username, setUsername] = useState('');
+  useEffect(() => {
+    const fetchUsername = async () => {
+      const user = await getUsername('username');
+      if(user){
+        setUsername(user);
+      }
+    };
+    fetchUsername();
+  }, []);
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {/* Header */}
       <Text style={styles.header}>GamerHUB Home</Text>
-      
+      <Text style={styles.subHeader}>Welcome, {username}</Text>    
       {/* News Feed Section */}
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>News Feed</Text>
